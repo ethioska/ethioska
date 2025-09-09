804f81210f37d0b0293341e721f9216b36b6504c
ethioska <ethioska@gmail.com>
2025-09-09 06:58:10 +0300

    Create group-management.js

---
+++ components/group-management.js
@@ -0,0 +1,6 @@
+export function renderGroupManagement(container) {
+    container.innerHTML = `
+        <h2>Group Management</h2>
+        <p>(Placeholder - Implement group creation, editing, deletion features)</p>
+    `;
+}
