98c22b65a2f2e80852a5d1f8fa3097e6d3620bfc
ethioska <ethioska@gmail.com>
2025-09-09 07:03:59 +0300

    Create admin-profile.js

---
+++ components/admin-profile.js
@@ -0,0 +1,6 @@
+export function renderAdminProfile(container) {
+    container.innerHTML = `
+        <h2>Admin Profile</h2>
+        <p>(Placeholder - Admin profile information and settings)</p>
+    `;
+}
